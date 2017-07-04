7170ee0959717fdb3f27bee80c85c84258e73d60
hewenxuan <hewenxuan@kf5.com>
2017-07-04 12:06:41 +0800

    dev:添加测试文件

---
+++ karma.conf.js
@@ -0,0 +1,20 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha', 'chai'],
+    files: ['test/**/*.js'],
+    reporters: ['progress'],
+    port: 9876,  // karma web server port
+    colors: true,
+    logLevel: config.LOG_INFO,
+    browsers: ['ChromeHeadless', 'MyHeadlessChrome'],
+    autoWatch: false,
+    // singleRun: false, // Karma captures browsers, runs the tests and exits
+    concurrency: Infinity,
+    customLaunchers:{
+      MyHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223']
+      }
+    },
+  })
+}
